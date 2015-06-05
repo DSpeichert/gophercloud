e84a2180360e18231867114f2ae6356c625031c7
Daniel Speichert <daniel_speichert@cable.comcast.com>
2015-06-05 10:32:27 -0400

    initial telemetry v2

--- openstack/client.go
+++ openstack/client.go
@@ -334,3 +334,13 @@ func NewDBV1(client *gophercloud.ProviderClient, eo gophercloud.EndpointOpts) (*
 	}
 	return &gophercloud.ServiceClient{ProviderClient: client, Endpoint: url}, nil
 }
+
+// NewTelemetryV2 creates a ServiceClient that may be used to access the v2 telemetry service.
+func NewTelemetryV2(client *gophercloud.ProviderClient, eo gophercloud.EndpointOpts) (*gophercloud.ServiceClient, error) {
+	eo.ApplyDefaults("metering")
+	url, err := client.EndpointLocator(eo)
+	if err != nil {
+		return nil, err
+	}
+	return &gophercloud.ServiceClient{ProviderClient: client, Endpoint: url}, nil
+}
